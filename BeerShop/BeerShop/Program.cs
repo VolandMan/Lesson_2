76525926b57e1317c42358bd17f2a9034fb46529
Voland <maloyvladis@gmail.com>
2019-09-18 22:17:37 +0300

    First commit

---
+++ BeerShop/BeerShop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BeerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PriceHoegarden = 70;
+            int PriceLeffe = 80;
+            int PriceStella = 60;
+            int PriceObolon = 7;
+            int MinSum = 6;
+
+            Console.WriteLine("Магазин BeerShop");
+            Console.WriteLine("Какую сумму готовы потратить?");
+
+            int money = Convert.ToInt32(Console.ReadLine());
+
+            if (money <= MinSum)
+            {
+                Console.WriteLine("Вашей суммы не достаточно для совершения покупок :(((");
+            }
+               else if (money > PriceLeffe)
+                {
+                int sum = money - PriceLeffe;
+                Console.WriteLine($"Лучшим выбором для вас будет Leffe. Сдача= {sum}");
+                }
+
+                  if (money <= PriceLeffe && money >= PriceHoegarden)
+                   {
+                   int sum = money - PriceHoegarden;
+                   Console.WriteLine($"Лучшим выбором для вас будет Hoegarden. Сдача= {sum}");
+                   }
+                     else if (money < PriceHoegarden && money >= PriceStella)
+                      {
+                      int sum = money - PriceStella;
+                      Console.WriteLine($"Лучшим выбором для вас будет Stella. Сдача= {sum}");
+                      }
+                         else if (money < PriceStella && money >= PriceObolon)
+                          {
+                          int sum = money - PriceObolon;
+                          Console.WriteLine($"Лучшим выбором для вас будет Obolon. Сдача= {sum}");
+                          }
+
+
+            Console.WriteLine("____________________ Спасибо за покупку! _________________");
+
+
+        }
+    }
+}
+    
+
---
+++ Sale/Sale/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SaleSum;
+            double Rezalt;
+
+            Console.WriteLine("Введите сумму покупки и нажмите Enter:");
+
+            double sum = Convert.ToInt32(Console.ReadLine());
+
+          if (sum > 0 && sum <= 300)
+           {
+                Console.WriteLine("Скидка не предусмотрена");
+           }
+               else if (sum > 300 && sum <= 400)
+               {
+                SaleSum = (sum / 100) * 2;
+                Rezalt = sum - SaleSum;
+                Console.WriteLine($"Сумма без скидки={sum}. Сумма со скидкой 2% = {Rezalt}");
+               }
+
+                  else if (sum > 400 && sum <= 500)
+                   {
+                   SaleSum = (sum / 100) * 3;
+                   Rezalt = sum - SaleSum;
+                   Console.WriteLine($"Сумма без скидки={sum}. Сумма со скидкой 3% = {Rezalt}");
+                   }
+
+                else if (sum > 500)
+                 {
+                 SaleSum = (sum / 100) * 5;
+                 Rezalt = sum - SaleSum; 
+                 Console.WriteLine($"Сумма без скидки={sum}. Сумма со скидкой 5% = {Rezalt}");
+                 }
+            Console.WriteLine("____________________ Спасибо за покупку! _________________");
+        }
+    }
+}
+
